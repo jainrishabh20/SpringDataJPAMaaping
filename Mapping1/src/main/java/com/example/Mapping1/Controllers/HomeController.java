1f3e73c2274bb0d08141ecf82d1b669a8969157f
Rishabh Jain <jrishabhjain20@gmail.com>
2023-10-27 18:50:17 +0530

    have done few changes in HomeController

--- Mapping1/src/main/java/com/example/Mapping1/Controllers/HomeController.java
+++ Mapping1/src/main/java/com/example/Mapping1/Controllers/HomeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 @Controller
-@RequestMapping("home")
+@RequestMapping("home") //Adding for git
 public class HomeController {
 
     @Autowired
